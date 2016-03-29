e9a0a20f2d3b3af518424ee20abb76bd3ac3dd13
androidYin <teacheryin.android@gmail.com>
2016-03-29 12:19:31 +0800

    Q_A - activity1

--- app/src/main/java/com/example/android/lab07_activities/Activity1.java
+++ app/src/main/java/com/example/android/lab07_activities/Activity1.java
@@ -3,13 +3,24 @@ package com.example.android.lab07_activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class Activity1 extends AppCompatActivity {
-    private TextView m_tv_main_activity_message;
+
+    public static final String Q1_ANSWER_KEY = "Q1";
+
+    private TextView m_tv_no;
+    private TextView m_tv_question;
+    private RadioButton m_radio_a;
+    private RadioButton m_radio_b;
+    private RadioButton m_radio_c;
+
     private CharSequence m_answer;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
@@ -17,24 +28,41 @@ public class Activity1 extends AppCompatActivity {
     }
 
     private void init() {
-        m_tv_main_activity_message = (TextView)findViewById(R.id.tv_main_activity_message);
+        m_tv_no = (TextView)findViewById(R.id.tv_no);
+        m_tv_question = (TextView)findViewById(R.id.tv_question);
+        m_radio_a = (RadioButton)findViewById(R.id.radio_a);
+        m_radio_b = (RadioButton)findViewById(R.id.radio_b);
+        m_radio_c = (RadioButton)findViewById(R.id.radio_c);
+
+        m_tv_no.setText("1");
+        m_tv_question.setText(Html.fromHtml(getString(R.string.question_1)));
+        m_radio_a.setText(Html.fromHtml(getString(R.string.question_1_radio_a)));
+        m_radio_b.setText(Html.fromHtml(getString(R.string.question_1_radio_b)));
+        m_radio_c.setText(Html.fromHtml(getString(R.string.question_1_radio_c)));
+
         initBackgroundColor();
+
     }
 
     private void initBackgroundColor() {
-        // 從 intent 取得傳來的顏色值，如果對方忘了放顏色值，我們會將顏色值設為 -1
         int color = getIntent().getIntExtra(ColorPickerActivity.BUNDLE_KEY_COLOR_INT, -1);
-        // 當顏色為為 -1 直接結束
         if(color == -1) {
             return;
         }
-        // 將傳來的顏色設定到 TextView
-        m_tv_main_activity_message.setBackgroundColor(color);
+        m_tv_question.setBackgroundColor(color);
     }
 
+
     public void next(View view) {
-        Intent intent = new Intent(this, Activity2.class);
+
+        String packageName = Activity2.class.getPackage().getName();
+        String className = Activity2.class.getName();
+        Intent intent = getIntent().setClassName(packageName, className);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        intent.putExtra(Q1_ANSWER_KEY, m_answer);
+        intent.putExtra("from","1");
         startActivity(intent);
     }
+
 
 }
